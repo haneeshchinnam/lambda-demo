c35ba65a3f04d2e45de067c98c9377240793dbd6
nakul-gopal-vas <nakul.gopal@partner.vas.com>
2024-11-26 19:15:08 +0530

    updated code

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,8 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+    print("implement")
+    print(event)
     return {
         'statusCode': 200,
         'body': json.dumps('Updated from Github!')
